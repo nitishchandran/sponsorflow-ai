78147b7fd2bc3865d01bbddfddf5d01aba514637
nilaashchandran <channilaash@gmail.com>
2026-06-03 01:26:25 +0530

    Day 1 - Spring Boot PostgreSQL setup

---
+++ src/main/java/com/nitish/sponsorflow/SponsorflowApplication.java
@@ -0,0 +1,13 @@
+package com.nitish.sponsorflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsorflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SponsorflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nitish/sponsorflow/SponsorflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitish.sponsorflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsorflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
